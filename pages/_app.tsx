c8c3635dd46bdbda8252e4a0f30a6f49e2eb71e8
Ricky Adriell <53133228+rickyadriell@users.noreply.github.com>
2025-01-01 21:37:31 +0000

    Clean up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import './global.css'
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function App({ Component, pageProps }) {
